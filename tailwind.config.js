9bd80c84cc310d9b5ba28fee010a0952c03090f4
st1020 <me@st1020.com>
2025-05-22 21:03:59 +0800

    feat: update tailwindcss to v4

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./templates/**/*.html"],
-  darkMode: "class",
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/typography")],
-};
